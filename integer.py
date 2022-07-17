37810acd134b100684dea49000d8cbb086409a39
gennychoi <gennychoi318@gmail.com>
2022-07-17 22:38:43 +0800

    second commit

--- integer.py
+++ integer.py
@@ -1,4 +1,2 @@
 days = int(input('Enter days: '))
-months = days // 30
-days = days % 30
-print('Months = {}, Days = {}'.format(months, days))
+print('Months = {}, Days = {}'.format(*divmod(days, 30)))
